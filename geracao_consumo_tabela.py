48bab95677550c0fdde20d2b268fe7a739173854
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2021-04-11 18:08:25 -0300

    Update geracao_consumo_tabela.py

--- geracao_consumo_tabela.py
+++ geracao_consumo_tabela.py
@@ -7,43 +7,72 @@ f = open("Anuário Estatístico de Energia Elétrica 2020 - Workbook.xlsx - Tabe
 # f.read lê todo o conteúdo do arquivo e retorna uma string
 # .split("\n") divide essa string do conteudo em linhas
 content = f.read().split("\n")
-years = list(map(int, content[8].split(",")[2:-4]))
+#lista vazia que armazena os anos
+years = []
+#for lê a oitava linha que contem os anos dentro do arquivo csv
+#.split(",") separa elementos por vírgula da lista, começando pelo elemento 2 e excluindo os últimos 4
+for y in content[8].split(",")[2:-4]:
+    #insere na lista years cada elemento de y transformado de string para inteiro
+    years.append (int(y))
+#lista vazia que armazena energia de cada região
 data = []
+#for lê todas as linhas da lista
 for l in content:
+    #.split(",") separa por vírgula
     ls = l.split(",")
-    if ls[1] == "Sudeste":
+    #condiciona que se o elemento 1 da lista for igual à "Norte" prossigo
+    if ls[1] == "Norte":
+        #determina o começo e fim da lista, partindo do elemento 2 e excluindo os últimos 5
         dt = ls[2:-5]
-        data = list(map(float, ls[2:-5]))
+        #lê cada elemento da lista filtrada
+        for l in dt:
+            #insere na lista data cada elemento l transformado de string para float
+            data.append(float(l))
         
-linha = go.Scatter(x = years,
-                   y = data, 
-                   mode = 'lines',
-                   name = 'Geracao [GWh]',
-                   line = {'color': '#38aecc'})
+#variável que armazena informações do gráfico       
+linha = go.Scatter(x = years, #anos do gráfico, eixo x
+                   y = data, #energia do gráfico, eixo y
+                   mode = 'lines', #modo do gráfico, tipo linhas
+                   name = 'Geracao [GWh]', #nome das linhas, no caso geração
+                   line = {'color': '#38aecc'}) #cor das linhas
 
 # abre o arquivo
 f = open("Anuário Estatístico de Energia Elétrica 2020 - Workbook.xlsx - Tabela 3.1.csv")
 # f.read lê todo o conteúdo do arquivo e retorna uma string
 # .split("\n") divide essa string do conteudo em linhas
 content = f.read().split("\n")
-years = list(map(int, content[8].split(",")[2:-4]))
+#insere na lista years cada elemento de y transformado de string para inteiro
+years = []
+#for lê a oitava linha que contem os anos dentro do arquivo csv
+#.split(",") separa elementos por vírgula da lista, começando pelo elemento 2 e excluindo os últimos 4
+for y in content[8].split(",")[2:-4]:
+    #insere na lista years cada elemento de y transformado de string para inteiro
+    years.append (int(y))
+#lista vazia que armazena energia de cada região
 data = []
+#for lê todas as linhas da lista
 for l in content:
+    #.split(",") separa por vírgula
     ls = l.split(",")
-    if ls[1] == "Sudeste":
+    #condiciona que se o elemento 1 da lista for igual à "Norte" prossigo
+    if ls[1] == "Norte":
+        #determina o começo e fim da lista, partindo do elemento 2 e excluindo os últimos 5
         dt = ls[2:-5]
-        data = list(map(float, ls[2:-5]))
+        #lê cada elemento da lista filtrada
+        for l in dt:
+            #insere na lista data cada elemento l transformado de string para float
+            data.append(float(l))
+#variável que armazena informações do gráfico
+barra = go.Bar(x = years,#anos do gráfico, eixo x
+                   y = data, #energia do gráfico, eixo y
+                   name = 'Consumo [GWh]',#nome das barras, no caso geração
+                   marker = {'color': '#183446'})#cor das barras
 
-barra = go.Bar(x = years,
-                   y = data, 
-                   name = 'Consumo [GWh]',
-                   marker = {'color': '#183446'})
 
+config = go.Layout( title ='Consumo Vs Geração [GWh]', #título do gráfico
+                                yaxis={'title':'Geração/Consumo [GWh]'}, #título eixo y
+                                xaxis={'title':''}) #título eixo x
+trace = [linha, barra] #variável que armazena gráficos
+fig = go.Figure(data=trace, layout=config) #transforma em fig os gráficos com o layout determinado
 
-config = go.Layout( title ='Consumo Vs Geração [GWh]', 
-                                yaxis={'title':'Geração/Consumo [GWh]'},
-                                xaxis={'title':''})
-trace = [linha, barra]
-fig = go.Figure(data=trace, layout=config)
-
-py.iplot(fig)
+py.iplot(fig) #plot fig
